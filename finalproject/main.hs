c8cacb454aa20cd9d57cf387206614e092e3133c
Tanner Bornemann <bornemtr@mail.uc.edu>
2017-12-03 15:13:28 -0500

    can now set color depth in newImage

--- finalproject/main.hs
+++ finalproject/main.hs
@@ -4,9 +4,8 @@ import Codec.Picture
 
 -- main
 -- > y wudi ly
--- main = putStrLn "y wudi ly"
-main = do
-    imageCreator "test4.png"
+main = do 
+    putStrLn "y wudi ly"
 
 -- 'imageCreator "test1.png"
 -- notes: first number is width, second is height
@@ -14,7 +13,8 @@ imageCreator :: String -> IO ()
 imageCreator path = writePng path $ generateImage pixelRenderer 250 250
    where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) 255
 
--- 'newImage "test1.png" 100 100
-newImage :: String -> Int -> Int -> IO ()
-newImage path width height = writePng path $ generateImage pixelRenderer width height
-    where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) 255
+-- 'newImage "test1.png" 100 100 128
+-- args: filename width height rgbDepth
+newImage :: String -> Int -> Int -> Pixel8 -> IO ()
+newImage path width height depth = writePng path $ generateImage pixelRenderer width height
+    where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) (fromIntegral depth)
