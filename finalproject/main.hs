2c3a8361a39e6f16a74afbefd6c5ac12f9704104
Tanner Bornemann <bornemtr@mail.uc.edu>
2017-12-03 15:00:44 -0500

    can create an image w/ custom width and height

--- finalproject/main.hs
+++ finalproject/main.hs
@@ -4,9 +4,17 @@ import Codec.Picture
 
 -- main
 -- > y wudi ly
-main = putStrLn "y wudi ly"
+-- main = putStrLn "y wudi ly"
+main = do
+    imageCreator "test4.png"
 
 -- 'imageCreator "test1.png"
+-- notes: first number is width, second is height
 imageCreator :: String -> IO ()
-imageCreator path = writePng path $ generateImage pixelRenderer 250 300
-   where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) 128
+imageCreator path = writePng path $ generateImage pixelRenderer 250 250
+   where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) 255
+
+-- 'newImage "test1.png" 100 100
+newImage :: String -> Int -> Int -> IO ()
+newImage path width height = writePng path $ generateImage pixelRenderer width height
+    where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) 255
