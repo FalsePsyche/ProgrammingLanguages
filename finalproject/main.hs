14912be321681481fe77d973f0a4092815ac121d
Tanner Bornemann <bornemtr@mail.uc.edu>
2017-12-04 20:59:54 -0500

    adding experimental thingies

--- finalproject/main.hs
+++ finalproject/main.hs
@@ -11,7 +11,7 @@ defaultWidth = (fromIntegral 255)
 defaultHeight = (fromIntegral 128)
 
 printer :: IO ()
-printer = print (fromIntegral 128)
+printer = print (fromIntegral defaultHeight)
 
 -- Creates a png with a cool color gradient
 -- 'imageCreator "test1.png"
@@ -40,27 +40,27 @@ newImage16 path width height blue = writePng path $ generateImage pixelRenderer 
 -- createPixelRGB16 red green blue = PixelRGB16 red green blue
 
 experiment :: String -> IO ()
-experiment path = writePng path $ generateImage renderAlgorithm 255 255
+experiment path = writePng path $ generateImage renderAlgorithm defaultWidth defaultHeight
 
 experiment2 :: String -> IO ()
-experiment2 path = writePng path $ generateImage renderAlgorithmModulo 255 255
+experiment2 path = writePng path $ generateImage renderAlgorithmModulo defaultWidth defaultHeight
 
 renderAlgorithm :: Int -> Int -> PixelRGB8
 renderAlgorithm x y = PixelRGB8 
-                        (fromIntegral (x - 128)) 
-                        (fromIntegral (y + 128)) 
-                        (fromIntegral (x+y + 128))
+                        (fromIntegral (x - defaultHeight)) 
+                        (fromIntegral (y + defaultHeight)) 
+                        (fromIntegral (x + y + defaultHeight))
 
 -- do something with mod
 
 renderAlgorithmModulo :: Int -> Int -> PixelRGB8
 renderAlgorithmModulo x y = PixelRGB8 
-                            (fromIntegral (mod (x - 128) 2) * 128) -- red
-                            (fromIntegral (mod (y + 128) 2) * 128) -- green
+                            (fromIntegral (mod (x - defaultHeight) 2) * 128) -- red
+                            (fromIntegral (mod (y + defaultHeight) 2) * 128) -- green
                             (fromIntegral (mod x (y + 1))) -- blue
 
 meltyIceCream :: String -> IO ()
-meltyIceCream path = writePng path $ generateImage renderAlgorithmMeltyIceCream 255 255
+meltyIceCream path = writePng path $ generateImage renderAlgorithmMeltyIceCream defaultWidth defaultHeight
 
 -- this render algorithm creates a mixed color that looks like it is melting or something weird
 renderAlgorithmMeltyIceCream :: Int -> Int -> PixelRGB8
@@ -68,3 +68,54 @@ renderAlgorithmMeltyIceCream x y = PixelRGB8
                         (fromIntegral (y + x^2 + 170)) -- red
                         (fromIntegral (y + x^2 + 85)) -- green
                         (fromIntegral (y + x^2)) -- blue
+
+-- one :: String -> IO ()
+-- one path = writePng path $ createMutableImage defaultWidth defaultWidth $ (renderAlgorithmone 50 50)
+
+renderAlgorithmone :: Int -> Int -> PixelRGB8
+renderAlgorithmone x y = PixelRGB8
+                        (fromIntegral (y + x^2 + 127)) -- red
+                        (fromIntegral (x^2 + 85)) -- green
+                        (fromIntegral (y + x^2)) -- blue
+
+two :: String -> IO ()
+two path = writePng path $ generateImage renderAlgorithmtwo defaultWidth defaultWidth
+
+renderAlgorithmtwo :: Int -> Int -> PixelRGB8
+renderAlgorithmtwo x y = PixelRGB8
+                        (fromIntegral (y)) -- red
+                        (fromIntegral (x)) -- green
+                        (fromIntegral (y + x + 127)) -- blue
+
+getArray :: Int -> [Int]
+getArray x
+    | x > 5 = []
+    | otherwise = [x..5]
+
+-- create a 'check neighbors' func that will check the next 3 pixels horizontally and vertically
+
+-- allColors :: Int -> [Int]
+-- allColors i = take 10 (i+2)
+
+-- takes a seed and reports a pseudo random value
+getAdjustedValue :: Int -> Int
+getAdjustedValue i = (fromIntegral (3 + (mod (7*i) 2)))
+
+-- will return a list of RGB tuples, the list will contain every RGB combination possible
+allColors :: [(Int, Int, Int)]
+allColors = [(x, y, z) | x <- [0..defaultWidth], y <- [0..defaultWidth], z <- [0..defaultWidth]]
+
+getR :: (Int, Int, Int) -> Int
+getR (r, _, _) = r
+
+getG :: (Int, Int, Int) -> Int
+getG (_, g, _) = g
+
+getB :: (Int, Int, Int) -> Int
+getB (_, _, b) = b
+
+renderAlgorithmPlain :: (Int, Int, Int) -> PixelRGB8
+renderAlgorithmPlain (r, g, b) = PixelRGB8
+                                (fromIntegral r)
+                                (fromIntegral g)
+                                (fromIntegral b)
