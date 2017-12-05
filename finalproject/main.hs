b1b429c09f8b2400a3c168f935efe72b4ddf734a
Tanner Bornemann <bornemtr@mail.uc.edu>
2017-12-04 20:33:42 -0500

    refactoring out numbers for default height and width vars

--- finalproject/main.hs
+++ finalproject/main.hs
@@ -7,8 +7,11 @@ import Codec.Picture
 main = do 
     putStrLn "y wudi ly"
 
+defaultWidth = 255
+defaultHeight = 128
+
 printer :: IO ()
-printer = print (fromIntegral 128)
+printer = print (fromIntegral defaultHeight)
 
 -- Creates a png with a cool color gradient
 -- 'imageCreator "test1.png"
@@ -17,8 +20,8 @@ printer = print (fromIntegral 128)
 -- holy shit pixelRenderer is the algorithm for the generateImage?? how does this fucking work WTF there isn't even anything telling to loop or iterate wtffffffff
 -- holy shit generateImage takes a function as its first parm, and calls the func with iterated values from 0 to 249 (width - 1) to produce the colors
 imageCreator :: String -> IO ()
-imageCreator path = writePng path $ generateImage pixelRenderer 255 255
-   where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) 128
+imageCreator path = writePng path $ generateImage pixelRenderer defaultWidth defaultWidth
+   where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) defaultHeight
 
 -- 'newImage "test1.png" 100 100 128
 -- args: filename width height blue
@@ -37,27 +40,25 @@ newImage16 path width height blue = writePng path $ generateImage pixelRenderer 
 -- createPixelRGB16 red green blue = PixelRGB16 red green blue
 
 experiment :: String -> IO ()
-experiment path = writePng path $ generateImage renderAlgorithm 255 255
+experiment path = writePng path $ generateImage renderAlgorithm defaultWidth defaultWidth
 
 experiment2 :: String -> IO ()
-experiment2 path = writePng path $ generateImage renderAlgorithmModulo 255 255
+experiment2 path = writePng path $ generateImage renderAlgorithmModulo defaultWidth defaultHeight
 
 renderAlgorithm :: Int -> Int -> PixelRGB8
 renderAlgorithm x y = PixelRGB8 
-                        (fromIntegral (x - 128)) 
-                        (fromIntegral (y + 128)) 
-                        (fromIntegral (x+y + 128))
-
--- do something with mod
+                        (fromIntegral (x - defaultHeight)) 
+                        (fromIntegral (y + defaultHeight)) 
+                        (fromIntegral (x+y + defaultHeight))
 
 renderAlgorithmModulo :: Int -> Int -> PixelRGB8
 renderAlgorithmModulo x y = PixelRGB8 
-                            (fromIntegral (mod (x - 128) 2) * 128) -- red
-                            (fromIntegral (mod (y + 128) 2) * 128) -- green
+                            (fromIntegral (mod (x - defaultHeight) 2) * defaultHeight) -- red
+                            (fromIntegral (mod (y^2) 55)) -- green
                             (fromIntegral (mod x (y + 1))) -- blue
 
 meltyIceCream :: String -> IO ()
-meltyIceCream path = writePng path $ generateImage renderAlgorithmMeltyIceCream 255 255
+meltyIceCream path = writePng path $ generateImage renderAlgorithmMeltyIceCream defaultWidth defaultWidth
 
 -- this render algorithm creates a mixed color that looks like it is melting or something weird
 renderAlgorithmMeltyIceCream :: Int -> Int -> PixelRGB8
