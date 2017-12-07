915bb36817dd14083513703467cc9cb3e18bc20e
Tanner Bornemann <bornemtr@mail.uc.edu>
2017-12-06 20:47:44 -0500

    adding stolen shuffle code that doesn't work for me

--- finalproject/main.hs
+++ finalproject/main.hs
@@ -122,8 +122,8 @@ allColors = [(r, g, b) | r <- [0, 8..255], g <- [0, 8..255], b <- [0, 8..255]]
 -- generates a striped rainbow image
 renderAllColor :: Int -> Int -> PixelRGB8
 renderAllColor x y = PixelRGB8
-                    (fromIntegral $ (getR (allColors!!(x*140))))
-                    (fromIntegral $ (getG (allColors!!(x*70))))
+                    (fromIntegral $ (getR (allColors!!(x*140)))) -- can't go higher than 140, or index too large
+                    (fromIntegral $ (getG (allColors!!(y*280))))
                     (fromIntegral $ (getB (allColors!!(x))))
 
 getR :: (Int, Int, Int) -> Int
@@ -140,3 +140,66 @@ renderAlgorithmPlain (r, g, b) = PixelRGB8
                                 (fromIntegral r)
                                 (fromIntegral g)
                                 (fromIntegral b)
+
+
+-- this code was stolen from http://okmij.org/ftp/Haskell/perfect-shuffle.txt
+
+-- A complete binary tree, of leaves and internal nodes.
+-- Internal node: Node card l r
+-- where card is the number of leaves under the node.
+-- Invariant: card >=2. All internal tree nodes are always full.
+data Tree a = Leaf a | Node !Int (Tree a) (Tree a) deriving Show
+
+-- Convert a non-empty sequence (e1...en) to a complete binary tree
+build_tree = grow_level . (map Leaf)
+    where
+    grow_level [node] = node
+    grow_level l = grow_level $ inner l
+	     
+    inner [] = []
+    inner x@[_] = x
+    inner (e1:e2:rest) = (join e1 e2) : inner rest
+	     
+    join l@(Leaf _)       r@(Leaf _)       = Node 2 l r
+    join l@(Node ct _ _)  r@(Leaf _)       = Node (ct+1) l r
+    join l@(Leaf _)       r@(Node ct _ _)  = Node (ct+1) l r
+    join l@(Node ctl _ _) r@(Node ctr _ _) = Node (ctl+ctr) l r
+
+{-
+-- example:
+Main> build_tree ['a','b','c','d','e']
+Node 5 (Node 4 (Node 2 (Leaf 'a') (Leaf 'b'))
+               (Node 2 (Leaf 'c') (Leaf 'd')))
+       (Leaf 'e')
+
+-}
+
+-- given a non-empty sequence (e1,...en) to shuffle, and a sequence
+-- (r1,...r[n-1]) of numbers such that r[i] is an independent sample
+-- from a uniform random distribution [0..n-i], compute the
+-- corresponding permutation of the input sequence.
+
+shuffle1 :: [a] -> [Int] -> [a]
+shuffle1 elements rseq = shuffle1' (build_tree elements) rseq
+    where
+    shuffle1' (Leaf e) [] = [e]
+    shuffle1' tree (ri:r_others) = extract_tree ri tree 
+				    (\tree -> shuffle1' tree r_others)
+	     -- extract_tree n tree
+	     -- extracts the n-th element from the tree and returns
+	     -- that element, paired with a tree with the element
+	     -- deleted (only instead of pairing, we use CPS)
+	     -- The function maintains the invariant of the completeness
+	     -- of the tree: all internal nodes are always full.
+	     -- The collection of patterns below is deliberately not complete.
+	     -- All the missing cases may not occur (and if they do,
+	     -- that's an error.
+    extract_tree 0 (Node _ (Leaf e) r) k = e:k r
+    extract_tree 1 (Node 2 l@Leaf{} (Leaf r)) k = r:k l
+    extract_tree n (Node c l@Leaf{} r) k =
+	extract_tree (n-1) r (\new_r -> k $ Node (c-1) l new_r)
+    extract_tree n (Node n1 l (Leaf e)) k | n+1 == n1 = e:k l
+				       
+    extract_tree n (Node c l@(Node cl _ _) r) k
+	| n < cl = extract_tree n l (\new_l -> k $ Node (c-1) new_l r)
+	| otherwise = extract_tree (n-cl) r (\new_r -> k $ Node (c-1) l new_r)
